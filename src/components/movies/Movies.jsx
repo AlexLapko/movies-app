4eab26e3ddb6d7e5cb4e790494bd4c7013bac3f8
AlekseyLapko <lesha.lapko.94@mail.ru>
2021-07-18 15:16:08 +0300

    refactoring pages, sort, movies components

--- src/components/movies/Movies.jsx
+++ src/components/movies/Movies.jsx
@@ -1,11 +1,10 @@
-import React, { Fragment, useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import Movie from './movie/Movie'
 import { getMovies } from '../../actions/movies'
 import './Movies.sass'
-import { setCurrentPage } from '../../reducers/moviesReducer'
-import { createPages } from '../../utils/pagesCreator'
 import Sort from '../sort/Sort'
+import Pages from '../pages/Pages'
 
 const Movies = () => {
 
@@ -13,16 +12,11 @@ const Movies = () => {
   const movies = useSelector(state => state.movies.items)
   const isFetching = useSelector(state => state.movies.isFetching)
   const currentPage = useSelector(state => state.movies.currentPage)
-  const pagesCount = useSelector(state => state.movies.pagesCount)
   const currentSortBy = useSelector(state => state.movies.currentSortBy)
-
-  const pages = []
-  createPages(pages, pagesCount, currentPage)
 
   useEffect(() => {
     dispatch(getMovies(currentPage, currentSortBy))
   }, [currentPage, currentSortBy])
-
 
   return (
     <div>
@@ -41,34 +35,8 @@ const Movies = () => {
           </div>
         }
       </div>
-      <div className="pages">
-        {
-          currentPage !== 1
-          ?
-          <Fragment>
-            <span className="page" onClick={() => dispatch(setCurrentPage(1))}>&#8249;&#8249;</span>
-            <span className="page" onClick={() => dispatch(setCurrentPage(currentPage - 1))}>&#8249;</span>
-          </Fragment>
-          :
-          ''
-        }
-        {
-          pages.map((page, index) => <span
-            key={index}
-            className={currentPage === page ? "current-page page" :"page"}
-            onClick={() => dispatch(setCurrentPage(page))}>{page}</span>)
-        }
-        {
-          currentPage !== pagesCount
-          ?
-          <Fragment>
-            <span className="page" onClick={() => dispatch(setCurrentPage(currentPage + 1))}>&#8250;</span>
-            <span className="page" onClick={() => dispatch(setCurrentPage(pagesCount))}>&#8250;&#8250;</span>
-          </Fragment>
-          :
-          ''
-        }
-      </div>
+
+      <Pages />
     </div>
   )
 }
